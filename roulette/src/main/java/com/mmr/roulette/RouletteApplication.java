98fb886071e99169519759c93d4c067de9a20108
MM Rahman <oncmmr@gmail.com>
2020-05-30 13:42:53 +0200

    master: load playes on application start run time

--- roulette/src/main/java/com/mmr/roulette/RouletteApplication.java
+++ roulette/src/main/java/com/mmr/roulette/RouletteApplication.java
@@ -1,13 +1,24 @@
 package com.mmr.roulette;
 
+import com.mmr.roulette.player.PlayerLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RouletteApplication {
+public class RouletteApplication implements ApplicationRunner {
+
+    @Autowired
+    PlayerLoader playerLoader;
 
     public static void main(String[] args) {
         SpringApplication.run(RouletteApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        playerLoader.load();
+    }
 }
---
+++ roulette/src/main/java/com/mmr/roulette/bet/PlayerBet.java
@@ -0,0 +1,4 @@
+package com.mmr.roulette.bet;
+
+public class PlayerBet {
+}
