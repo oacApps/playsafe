38e136ed7c1062ddbf1a19e05fd5eced7c286e49
MM Rahman <oncmmr@gmail.com>
2020-05-30 12:46:50 +0200

    master: implemented controller to convert from kelvin to celsius

--- measurement/src/main/java/com/mmr/measurement/controller/ConversionsController.java
+++ measurement/src/main/java/com/mmr/measurement/controller/ConversionsController.java
@@ -1,4 +1,33 @@
 package com.mmr.measurement.controller;
 
+
+import com.mmr.measurement.dto.ConversionDTO;
+import com.mmr.measurement.enums.MeasurementEnum;
+import com.mmr.measurement.enums.UnitEnum;
+import com.mmr.measurement.mapper.MapTo;
+import com.mmr.measurement.service.MeasurementConversionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/conversions")
 public class ConversionsController {
+
+    @Autowired
+    MeasurementConversionService conversionService;
+
+    @Autowired
+    MapTo mapTo;
+
+    @GetMapping("/ktoc/{unitValue}")
+    public ResponseEntity<Double> kelvinToCelsius(@PathVariable double unitValue){
+        ConversionDTO conversionDTO = mapTo.conversionDTO(MeasurementEnum.TEMPERATURE, UnitEnum.KELVIN, UnitEnum.CELSIUS, unitValue);
+        double convertedValue = conversionService.convert(conversionDTO);
+        return new ResponseEntity<>(convertedValue, HttpStatus.OK);
+    }
 }
--- measurement/src/main/java/com/mmr/measurement/mapper/MapTo.java
+++ measurement/src/main/java/com/mmr/measurement/mapper/MapTo.java
@@ -1,4 +1,19 @@
 package com.mmr.measurement.mapper;
 
+import com.mmr.measurement.dto.ConversionDTO;
+import com.mmr.measurement.enums.MeasurementEnum;
+import com.mmr.measurement.enums.UnitEnum;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MapTo {
+
+    public ConversionDTO conversionDTO(MeasurementEnum measurementEnum, UnitEnum from, UnitEnum to, double value){
+        ConversionDTO conversionDTO = new ConversionDTO();
+        conversionDTO.setMeasurement(measurementEnum);
+        conversionDTO.setFrom(from);
+        conversionDTO.setTo(to);
+        conversionDTO.setValue(value);
+        return conversionDTO;
+    }
 }
